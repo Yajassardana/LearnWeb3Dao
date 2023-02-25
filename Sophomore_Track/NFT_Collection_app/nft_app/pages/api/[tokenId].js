9f77e5c706edd59631ad2264d78e5122a6e9a235
Yajassardana <yajassardana@gmail.com>
2023-02-26 02:39:15 +0530

    Comply with opensea requirements

--- Sophomore_Track/NFT_Collection_app/nft_app/pages/api/[tokenId].js
+++ Sophomore_Track/NFT_Collection_app/nft_app/pages/api/[tokenId].js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+      "https://i.ytimg.com/vi/2tynWkKEG1Q/maxresdefault.jpg";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
@@ -11,6 +11,6 @@ export default function handler(req, res) {
     res.status(200).json({
       name: "Bhussi NFT #" + tokenId,
       description: "Bhussi NFT is a collection of Seedhe maut stans",
-      image: image_url + tokenId + ".svg",
+      image: image_url + tokenId + ".jpg",
     });
   }
