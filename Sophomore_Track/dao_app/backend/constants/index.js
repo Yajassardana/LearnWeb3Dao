05f9f9220f81fd29f9b96b3523defc0a703053b1
Yajassardana <yajassardana@gmail.com>
2023-03-01 15:54:29 +0530

    Complete DAO app

---
+++ Sophomore_Track/dao_app/backend/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x7adacc60Aeb22bA9e6663a46839B74D0Ea32Eae6";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
--- Sophomore_Track/whitelist_dapp/hardhat_src/contracts/Whitelist.sol
+++ Sophomore_Track/whitelist_dapp/hardhat_src/contracts/Whitelist.sol
@@ -1,37 +1,42 @@
 //SPDX-License-Identifier: Unlicense
-
-/*
-1. Max whitelisted adderesses
-2. Hashmap of whitelisted addresses
-3. Num of addresses whitelisted
-4. pass 1 to constructor
-5. function to add address to whielist
-*/
 pragma solidity ^0.8.0;
 
 contract Whitelist {
-    uint8 public maxWhitelistedAddr;
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
 
-    mapping(address => bool) public whitelistedAddr;
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
+    mapping(address => bool) public whitelistedAddresses;
 
-    uint8 public numAddrWhitelisted;
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    // NOTE: Don't change this variable name, as it will be part of verification
+    uint8 public numAddressesWhitelisted;
 
-    constructor(uint8 _maxWhitelistedAddr) {
-        maxWhitelistedAddr = _maxWhitelistedAddr;
+    // Setting the Max number of whitelisted addresses
+    // User will put the value at the time of deployment
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
     }
 
-    function addAddrToWhitelist() public {
+    /**
+        addAddressToWhitelist - This function adds the address of the sender to the
+        whitelist
+     */
+    function addAddressToWhitelist() public {
+        // check if the user has already been whitelisted
         require(
-            !whitelistedAddr[msg.sender],
-            "Address already in the whitelist"
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
         );
-
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
         require(
-            maxWhitelistedAddr > numAddrWhitelisted,
-            "Maximimum whitelist limit reached"
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "More addresses cant be added, limit reached"
         );
-
-        whitelistedAddr[msg.sender] = true;
-        numAddrWhitelisted += 1;
+        // Add the address which called the function to the whitelistedAddress array
+        whitelistedAddresses[msg.sender] = true;
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
     }
 }
